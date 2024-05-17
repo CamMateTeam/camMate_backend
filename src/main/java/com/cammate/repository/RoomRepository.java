3bdcb29bcc026edddda0bc4ed71cf95cba4f1081
kite707 <12201680@inha.edu>
2024-05-18 01:21:53 +0900

    feat: RoomRepository 인터페이스 정의

---
+++ src/main/java/com/cammate/repository/RoomRepository.java
@@ -0,0 +1,15 @@
+package com.cammate.repository;
+
+import com.cammate.domain.Room;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomRepository {
+    //방 만들기. 찾기, 삭제
+    Room saveRoom(Room room);
+    Optional<Room>findRoomById(String id);
+    Optional<Room>findRoomByName(String nickname);
+    List<Room> findAll();
+    List<Room> findWithMacAddress(List<String> macAddressList);
+}
