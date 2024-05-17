8326fc63e75b9b63386e5a8b0eb482827f4cae43
kite707 <12201680@inha.edu>
2024-05-17 23:44:14 +0900

    initial commit

---
+++ src/main/java/com/cammate/CamMateApplication.java
@@ -0,0 +1,13 @@
+package com.cammate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cammate/CamMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cammate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
