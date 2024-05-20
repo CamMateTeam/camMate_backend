dc995a1939c93283668aeb6eb5a7d2eb13705bbb
kite707 <12201680@inha.edu>
2024-05-21 02:27:33 +0900

    feat: dto 정의

---
+++ src/main/java/com/cammate/domain/FindRoomDto.java
@@ -0,0 +1,9 @@
+package com.cammate.domain;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class FindRoomDto {
+     List<String> macAddressList;
+}
---
+++ src/main/java/com/cammate/domain/MakeRoomDto.java
@@ -0,0 +1,11 @@
+package com.cammate.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MakeRoomDto {
+    String nickname;
+    String password;
+    String macAddress;
+}
