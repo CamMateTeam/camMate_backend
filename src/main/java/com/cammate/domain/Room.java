12b28ca3574008a104385272d6e54ee5d18d8afe
kite707 <12201680@inha.edu>
2024-05-22 00:02:53 +0900

    feat: room테이블 생성

--- src/main/java/com/cammate/domain/Room.java
+++ src/main/java/com/cammate/domain/Room.java
@@ -1,11 +1,23 @@
 package com.cammate.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@Entity
+@Table(name="Room")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
     private String roomId;
     private String nickname;
     private String macAddress;
